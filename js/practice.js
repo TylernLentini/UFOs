70c96ff01ad99df38ac20111159f29c91ab71f0e
Tyler Lentini <tylernlentini@gmail.com>
2021-08-23 16:31:08 -0400

    module 11.5.3

---
+++ js/practice.js
@@ -0,0 +1,13 @@
+let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+
+// create loop to iterate through an array in JS
+function listLoop(userList) {
+    for (var i = 0; i < userList.length; i++) {
+      console.log(userList[i]);
+    }
+ }
+
+ // call the function
+ listLoop(friends);
+
+ //returns each name as an object
