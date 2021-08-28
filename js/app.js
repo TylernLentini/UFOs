b71d42b0ae1600c7a8ac7765d89431f86db71df7
Tyler Lentini <tylernlentini@gmail.com>
2021-08-28 12:54:44 -0400

    mod 11 challenge complete

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-filters = {}
+var filters = {}
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
@@ -45,7 +45,7 @@ function updateFilters() {
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
     if (Value) {
-      filters.append(filterId, Value);
+      filters[filterId] = Value;
     }
     else {
       delete filters[filterId];
@@ -61,18 +61,86 @@ function updateFilters() {
     console.log(filters);
   
     // 8. Set the filtered data to the tableData.
+    let filteredData = tableData 
     
-  
+    let filteredData2 = []
     // 9. Loop through all of the filters and keep any data that
-    // matches the filter values
-    
+    var filtersUsed = Object.keys(filters)
+    //console.log(filtersUsed);
+    if (filtersUsed.includes("state")){
+      for (let index in filteredData){
+        //console.log(filteredData[index])
+        let sighting = filteredData[index]
+        if (sighting.state === filters.state){
+           filteredData2 = filteredData.filter(function(sighting){
+            return (sighting.state === filters["state"])
+           })   
+        }
+      }
+    }
+    if (filtersUsed.includes("city")){
+      for (let index in filteredData){
+        let sighting = filteredData[index]
+        if (sighting.city === filters.city){
+           filteredData2 = filteredData.filter(function(sighting){
+            return (sighting.city === filters["city"])
+           })   
+        }
+      }
+    }
+    if (filtersUsed.includes("country")){
+      for (let index in filteredData){
+        let sighting = filteredData[index]
+        if (sighting.country === filters.country){
+           filteredData2 = filteredData.filter(function(sighting){
+            return (sighting.country === filters["country"])
+           })   
+        }
+      }
+    }
+    if (filtersUsed.includes("datetime")){
+      for (let index in filteredData){
+        let sighting = filteredData[index]
+        if (sighting.datetime === filters.datetime){
+           filteredData2 = filteredData.filter(function(sighting){
+            return (sighting.datetime === filters["datetime"])
+           })   
+        }
+      }
+    }
+    if (filtersUsed.includes("shape")){
+      for (let index in filteredData){
+        let sighting = filteredData[index]
+        if (sighting.shape === filters.shape){
+           filteredData2 = filteredData.filter(function(sighting){
+            return (sighting.shape === filters["shape"])
+           })   
+        }
+      }
+    }
+    if (filtersUsed.length === 0){
+     filteredData2 = tableData
+    }
+
+    console.log(filteredData2);
+
   
+    //const filteredData2 = filteredData.filter(function(sighting){
+      //return (sighting.state === filters["state"] && sighting.shape === filters["shape"] && \
+              //sighting.city === filters["city"] && sighting.country === filters["country"] && sighting.datetime === filters["datetime"] )
+
+    //});
+    //console.log(filteredData2);
+
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filteredData2);
   }
   
   // 2. Attach an event to listen for changes to each filter
   d3.selectAll("input").on("change", updateFilters);
   
   // Build the table when the page loads
-  buildTable(tableData);
+  buildTable(tableData); 
+
+
+  
